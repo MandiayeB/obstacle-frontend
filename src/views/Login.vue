b174f54acae3fa158cfd1cb8ea171b8d689c271b
BADIANE Mandiaye <badiane@et.intechinfo.fr>
2021-05-17 11:35:29 +0200

    Centralizing code in Login.vue
    After the merge from activities_page, I changed a function in Login.vue.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,35 +1,39 @@
 <template>
     <div class="ajustement_contenent">
-        <div class="cadre">
-            <!-- <h1>{{ User.firstname }}</h1> -->
+        <div class="cadre_login">
+            <div class="title_signin">
+                <h5 class="h5_title_signin">
+                    Connexion
+                </h5>
+            </div>
+            <div class="separateur"></div>
             <form @submit.prevent="handleSubmit" action="" method="post">
                 <div class="cadreInput">
                     <input
-                    type="text"
-                    v-model="email"
-                    class="inputCadre tailleInput"
-                    name="email"
-                    placeholder="Adresse mail"
-                    autocomplete="off"
-                    required
+                        type="text"
+                        v-model="email"
+                        class="inputCadre tailleInput"
+                        name="email"
+                        placeholder="Adresse mail"
+                        autocomplete="off"
+                        required
                     />
                 </div>
-                <br />
                 <div class="cadreInput">
                     <input
-                    type="password"
-                    v-model="password"
-                    class="inputCadre tailleInput"
-                    name="password"
-                    id="pass"
-                    placeholder="Mot de passe"
-                    autocomplete="off"
-                    required
+                        type="password"
+                        v-model="password"
+                        class="inputCadre tailleInput"
+                        name="password"
+                        id="pass"
+                        placeholder="Mot de passe"
+                        autocomplete="off"
+                        required
                     />
                 </div>
                 <div class="unButton">
                     <button value="" class="designButton" type="submit">
-                    Connexion
+                        Se connecter
                     </button>
                 </div>
             </form>
@@ -38,9 +42,11 @@
             </div>
             <div class="separateur"></div>
             <div class="unButton">
-                <button value="" class="designButton" type="submit">
-                    Créer un compte
-                </button>
+                <form @submit.prevent="redirectToSignin" action="" method="post">
+                    <button value="" class="designButton" type="submit">
+                        Créer un compte
+                    </button>
+                </form>
             </div>
         </div>
     </div>
@@ -50,32 +56,31 @@
 import axios from "axios";
 
 export default {
-  name: "Login",
-  data() {
-    return {
-      email: "",
-      password: "",
-    };
-  },
+    name: "Login",
+    data() {
+        return {
+            email: "",
+            password: "",
+        };
+    },
   methods: {
     handleSubmit() {
-      const data = {
-        email: this.email,
-        password: this.password,
-      };
-      axios
-        .post("http://localhost:3000/login", {
-          email: this.email,
-          password: this.password,
-        })
-        .catch((error) => {
-          if (error.response.status === 308 || error.response.status === 307) {
-            this.$router.push("/");
-          } else {
-            console.log(error);
-          }
-        });
+        axios
+            .post("http://localhost:3000/login", {
+                email: this.email,
+                password: this.password,
+            })
+            .catch((error) => {
+                if (error.response.status === 308 || error.response.status === 307) {
+                    this.$router.push("/");
+                } else {
+                    console.log(error);
+                }
+            });
     },
+    redirectToSignin() {
+        this.$router.push("/signin");
+    }
   },
 };
 </script>

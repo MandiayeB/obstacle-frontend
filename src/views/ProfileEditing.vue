8f157b09fe26092685c3451a2212fdb6eff4dfb8
VONG Allane <vong@et.intechinfo.fr>
2021-06-14 19:17:49 +0200

    Add editPassword for change your password
    Now you can edit credentials and password

--- src/views/ProfileEditing.vue
+++ src/views/ProfileEditing.vue
@@ -3,8 +3,10 @@
         <h1 id="homepage_title">Profil</h1>
         <div class="separateur"></div>
             <div class="picture_profil">
-                <img :src="user.image" :alt="user.firstname" />
+                <img :src="state.user.image" :alt="state.user.firstname" />
             </div>
+            <input type ="file" @change="onFileSelected">
+            <img id="myImg" src="#">
         <div class ="cadreCredentials">
             <div class="cadreInput">
                 <h3 class ="ajustementCredentials">Prénom </h3>
@@ -12,9 +14,12 @@
                     type="text"
                     class="inputCadre tailleInput adjumentmentInputPrenom"
                     name="fistname"
-                    :placeholder="user.firstname"
-                    v-model="firstname"
+                    :placeholder="state.user.firstname"
+                    v-model="state.fnQuery"
                 />
+                <span class="name_span" v-if="v$.fnQuery.$error">
+                    {{ v$.fnQuery.$errors[0].$message }}
+                </span>
             </div>
             <div class="cadreInput">
                 <h3 class="ajustementCredentials">Nom </h3>
@@ -22,9 +27,12 @@
                     type="text"
                     class="inputCadre tailleInput adjumentmentInputNom"
                     name="lastname"
-                    :placeholder="user.lastname"
-                    v-model="lastname"
+                    :placeholder="state.user.lastname"
+                    v-model="state.lnQuery"
                 />
+                <span class="name_span" v-if="v$.lnQuery.$error">
+                    {{ v$.lnQuery.$errors[0].$message }}
+                </span>
             </div>
             <div class="cadreInput">
                 <h3 class="ajustementCredentials">Email </h3>
@@ -32,9 +40,12 @@
                     type="text"
                     class="inputCadre tailleInput adjumentmentInputEmail"
                     name="email"
-                    :placeholder="user.email"
-                    v-model="email"
+                    :placeholder="state.user.email"
+                    v-model="state.emailQuery"
                 />
+                <span v-if="v$.emailQuery.$error">
+                    {{ v$.emailQuery.$errors[0].$message }}
+                </span>
             </div>
         </div>
         <div class="emplacementButton">
@@ -54,11 +65,33 @@
 
 <script>
 import axios from "axios";
+import useValidate from '@vuelidate/core';
+import { required, minLength, maxLength, sameAs, helpers } from '@vuelidate/validators';
+import { reactive, computed } from 'vue';
+ 
+const firstnameRegex = value => {
+    if (typeof value === 'undefined' || value === null || value === '') {
+        return true
+    }
+    return /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(value)
+}
+const lastnameRegex = value => {
+    if (typeof value === 'undefined' || value === null || value === '') {
+        return true
+    }
+    return /\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/.test(value)
+}
+const mailAdressRegex = value => {
+    if (typeof value === 'undefined' || value === null || value === '') {
+        return true
+    }
+    return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(value)
+}
 
 export default {
     name: "Setting",
-    data() {
-        return {
+    setup() {
+        const state = reactive({
             user: {
                 firstname: sessionStorage.getItem("firstname"),
                 lastname: sessionStorage.getItem("lastname"),
@@ -66,12 +99,31 @@ export default {
                 role: sessionStorage.getItem("role"),
                 image:"https://photos.lci.fr/images/613/344/kanyewest2020afp-5b6354-0@1x.jpeg",
             },
-            firstname: "",
-            lastname:"",
-            email:""
-
-        };
-        
+            fnQuery: "",
+            lnQuery:"",
+            emailQuery:""
+        })     
+        const rules = computed(() => {
+            return {
+                fnQuery: { 
+                    minLength: helpers.withMessage('*Votre prénom doit être long de 2 lettres minimum.', minLength(2)),
+                    firstnameRegex: helpers.withMessage('*Il manque une majuscule au début du mot.', firstnameRegex),
+                },
+                lnQuery: { 
+                    maxLength: helpers.withMessage('*Votre nom ne doit pas dépasser 20 lettres.',maxLength(20)), 
+                    lastnameRegex: helpers.withMessage('*Il manque une majuscule au début du mot.', lastnameRegex),
+                },
+                emailQuery: { 
+                    mailAdressRegex: helpers.withMessage("*L'adresse email n'est pas valide.", mailAdressRegex),
+                },
+            }
+        })
+        const v$ = useValidate(rules, state);
+        return{
+            state, 
+            v$,
+            selectedFile: null,
+        }
     },
     beforeCreate() {
         if (!sessionStorage.getItem("isAuthenticated")) {
@@ -82,30 +134,51 @@ export default {
         
     },
     methods: {
+        onFileSelected(event) {
+            console.log(event.target.files[0]);
+            this.selectedFile = event.target.files[0];
+        },
+        changePicture() {
+
+               if(this.files && this.files[0]) {
+                   var img = document.querySelector('img');
+                   img.onload=() => {
+                       URL.revokeObjectURL(img.src);
+                   }
+                   img.src = URL.createObjectURL(this.files[0]);
+                   console.log('LALSASLASLA')
+               }
+        },
         editing(){
-            axios
-                .put("http://localhost:3000/profile", {
-                    emailsession: sessionStorage.getItem("email"),
-                    firstname: this.firstname,
-                    lastname: this.lastname,
-                    email: this.email,
-                    
-                })
-                .catch((error) => {
-                    if(error.response.status === 308){
-                        this.$router.push('/profile');
-                        if(this.email!==""){
-                            sessionStorage.setItem('email', this.email);
+           
+            this.v$.$validate()
+            if(!this.v$.$error){
+                axios
+                    .put("http://localhost:3000/profile/editCredentials", {
+                        emailsession: sessionStorage.getItem("email"),
+                        firstname: this.state.fnQuery,
+                        lastname: this.state.lnQuery,
+                        email: this.state.emailQuery,
+                        
+                    })
+                    .catch((error) => {
+                        if(error.response.status === 308){
+                            this.$router.push('/profile');
+                            if(this.state.emailQuery!==""){
+                                sessionStorage.setItem('email', this.state.emailQuery);
+                            }
+                            if(this.state.fnQuery!==""){
+                                sessionStorage.setItem('firstname', this.state.fnQuery);
+                            }
+                            if(this.state.lnQuery!==""){
+                                sessionStorage.setItem('lastname', this.state.lnQuery);
+                            }
                         }
-                        if(this.firstname!==""){
-                            sessionStorage.setItem('firstname', this.firstname);
-                        }
-                        if(this.lastname!==""){
-                            sessionStorage.setItem('lastname', this.lastname);
-                        }
-                    }
-                    
-                });
+                        
+                    });
+            } else {
+                console.log(this.v$.$errors);
+            }   
         }
     }
 };
--- src/views/ProfilePassword.vue
+++ src/views/ProfilePassword.vue
@@ -3,35 +3,44 @@
         <h1 id="homepage_title">Changer votre mot de passe</h1>
         <div class="separateur"></div>
         <div class ="cadreCredentials">
-            <div class="cadreInput">
+            <div class="cadreoldPassword cadreProdil cadreInput">
                 <h3 class ="ajustementCredentials">Ancien mot de passe </h3>
                 <input
                     type="password"
-                    class="inputCadre tailleInput adjumentmentInputPrenom"
+                    class="inputCadre tailleInput adjumentmentPassword"
                     name="password"
                     placeholder="Ancien mot de passe"
-                    v-model="oldPassword"
+                    v-model="state.password.oldPassword"
                 />
+                <span v-if="v$.password.oldPassword.$error">
+                    {{ v$.password.oldPassword.$errors[0].$message }}
+                </span>
             </div>
-            <div class="cadreInput">
+            <div class="cadreProfil cadreInput">
                 <h3 class="ajustementCredentials">Nouveau mot de passe </h3>
                 <input
                     type="password"
-                    class="inputCadre tailleInput adjumentmentInputNom"
+                    class="inputCadre tailleInput adjumentmentPassword"
                     name="newPassword"
                     placeholder="Nouveau mot de passe"
-                    v-model="newPassword"
+                    v-model="state.password.newPassword"
                 />
+                <span v-if="v$.password.newPassword.$error">
+                    {{ v$.password.newPassword.$errors[0].$message }}
+                </span>
             </div>
-            <div class="cadreInput">
+            <div class="cadreProfil cadreInput">
                 <h3 class="ajustementCredentials">Confirmer votre nouveau mot de passe </h3>
                 <input
-                    type="text"
-                    class="inputCadre tailleInput adjumentmentInputEmail"
+                    type="password"
+                    class="inputCadre tailleInput adjumentmentPassword"
                     name="newpassword"
                     placeholder="Confirmer votre nouveau mot de passe"
-                    v-model="confirmPassword"
+                    v-model="state.password.confirmPassword"
                 />
+                <span v-if="v$.password.confirmPassword.$error">
+                    {{ v$.password.confirmPassword.$errors[0].$message }}
+                </span>
             </div>
         </div>
         <div class="emplacementButton">
@@ -46,33 +55,74 @@
 
 <script>
 import axios from "axios";
+import useValidate from '@vuelidate/core';
+import { required, minLength, maxLength, sameAs, helpers } from '@vuelidate/validators';
+import { reactive, computed } from 'vue';
+
+const passwordRegex = value => {
+    if (typeof value === 'undefined' || value === null || value === '') {
+        return true
+    }
+    return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(value)
+}
 
 export default {
     name: "EditPassword",
-    data() {
-        return {
-            oldPassword: "",
-            newPassword:"",
-            confirmPassword:""
-
-        };
+    setup() {
+        const state = reactive({
+            password:{
+                oldPassword: "",
+                newPassword:"",
+                confirmPassword:""
+            },
+        })
+        const rules = computed(() => {
+            return {
+                password: {
+                    oldPassword: { 
+                        required: helpers.withMessage('*Champ obligatoire.',required),
+                        passwordRegex: helpers.withMessage('*Le mot de passe doit faire 8 caractères au moins, au moins une lettre, un chiffre et un caractère spécial.', passwordRegex), 
+                    },
+                    confirmPassword: { 
+                        required: helpers.withMessage('*Champ obligatoire.',required), 
+                        passwordRegex: helpers.withMessage('*Le mot de passe doit faire 8 caractères au moins, au moins une lettre, un chiffre et un caractère spécial.', passwordRegex), 
+                        sameAs: helpers.withMessage('*Les mots de passes doivent correspondre.',sameAs(state.password.newPassword)), 
+                    },
+                    newPassword: { 
+                        required: helpers.withMessage('*Champ obligatoire.',required), 
+                        passwordRegex: helpers.withMessage('*Le mot de passe doit faire 8 caractères au moins, au moins une lettre, un chiffre et un caractère spécial.', passwordRegex),
+                    },
+                },
+            }
+        })
+        const v$ = useValidate(rules, state);   
+            return{
+            state, 
+            v$,
+        }
         
     },
     methods: {
         editing(){
-            axios
-                .put("http://localhost:3000/profile", {
-                    emailsession: sessionStorage.getItem("email"),
-                    oldPassword: this.oldPassword,
-                    newPassword: this.newPassword,
-                    confirmPassword: this.confirmPassword
-                })
-                .catch((error) => {
-                    if(error.response.status === 308){
-                        this.$router.push('/profile');
-                    }
-                    
-                });
+            this.v$.$validate();
+            if(!this.v$.$error){
+                console.log('Hello?');
+                axios
+                    .put("http://localhost:3000/profile/editPassword", {
+                        emailsession: sessionStorage.getItem("email"),
+                        oldPassword: this.state.password.oldPassword,
+                        newPassword: this.state.password.newPassword,
+                        confirmPassword: this.state.password.confirmPassword
+                    })
+                    .catch((error) => {
+                        if(error.response.status === 308){
+                            this.$router.push('/profile');
+                        }
+                        
+                    });
+            } else {
+                console.log(this.v$.$errors);
+            }   
         }
     }
 };

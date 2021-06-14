3dd4fec8afecdb0f1f206020e789d8de71bca76d
Le FUR François <flefur@et.intechinfo.fr>
2021-06-14 22:45:49 +0200

    Implemented profile page style.
    Changed profile page style to have a more responsive and styled page.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,20 +1,34 @@
 <template>
-    <div class="profile">
-        <ProfileHeader :user="user"/>
-        <ProfileBody :user="user" />
+    <div class="container_profile">
+        <div id="profile_header">
+            <h1 id="profile_title">Profil</h1>
+            <router-link id="profile_setting_wheel" :to="'/profile/edit'">
+                <img id="profile_setting_wheel_img" src="../assets/images/settings.png" />
+            </router-link>
+        </div>
+        <div class="separateur"></div>
+            <div class="profile_picture">
+                <img id="profile_picture" :src="user.image" :alt="user.firstname" />
+            </div>
+        <div class ="profile_frame">
+            <div class="profile_credential_frame">
+                <h3 class ="profile_page_h3">Prénom :</h3>
+                <h3 class="profile_page_h3">Nom :</h3>
+                <h3 class="profile_page_h3">Email :</h3>
+            </div>
+            <div class="profile_credential_frame">
+                <p class="profile_page_p">{{ user.firstname }}</p>
+                <p class="profile_page_p">{{ user.lastname }}</p>
+                <p class="profile_page_p">{{ user.email }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import ProfileHeader from '../components/ProfileHeader';
-import ProfileBody from '../components/ProfileBody';
 
 export default {
     name: 'Profile',
-    components: {
-        ProfileHeader,
-        ProfileBody
-    },
     data() {
         return {
             user: Object
@@ -37,3 +51,4 @@ export default {
     },
 }
 </script>
+
